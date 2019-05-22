a8ec7483ac613fc171bc6683e4772c5e5159bd17
EDOUGA jean patrick <jean.edouga@jassarl.com>
2019-05-22 10:15:20 +0100

    other

--- src/main/java/com/shopping_list/controller/HomeController.java
+++ src/main/java/com/shopping_list/controller/HomeController.java
@@ -3,6 +3,9 @@ package com.shopping_list.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 @Controller
 public class HomeController {
     @GetMapping("/")
@@ -14,5 +17,12 @@ public class HomeController {
     public String showLoginForm() {
 
         return "login";
+    }
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request){
+
+        HttpSession session = request.getSession();
+        session.invalidate();
+        return "redirect:/login";
     }
 }
